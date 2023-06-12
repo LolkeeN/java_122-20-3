fcafb3b1eafaf8d7b1c58566372d14eae2793e78
Vasyl Veselov <vasyl.veselov@flexychain.com>
2023-06-12 22:51:32 +0300

    completed lab5

---
+++ src/main/java/resources/init_students_table.sql
@@ -0,0 +1,40 @@
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd', 'qwe', 'zxc', '1999-04-20', 123);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd1', 'qwe2', 'zxc3', '1999-03-20', 124);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd3', 'qwe3', 'zxc3', '1999-03-20', 121);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd4', 'qwe4', 'zxc4', '1999-03-20', 120);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd5', 'qwe5', 'zxc5', '1999-02-20', 119);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd6', 'qwe6', 'zxc6', '1999-02-20', 111);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd7', 'qwe7', 'zxc7', '1999-01-20', 126);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd8', 'qwe8', 'zxc8', '1999-05-20', 127);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd9', 'qwe9', 'zxc9', '1999-07-20', 128);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd10', 'qwe10', 'zxc10', '1999-08-20', 129);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd11', 'qwe11', 'zxc11', '1999-09-20', 130);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd12', 'qwe12', 'zxc12', '1999-10-20', 131);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd13', 'qwe13', 'zxc13', '1999-11-20', 132);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd14', 'qwe14', 'zxc14', '1999-12-20', 133);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd15', 'qwe15', 'zxc15', '2000-01-21', 134);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd16', 'qwe16', 'zxc16', '2000-02-21', 135);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd17', 'qwe17', 'zxc17', '2000-02-21', 136);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd18', 'qwe18', 'zxc18', '2000-03-21', 137);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd19', 'qwe19', 'zxc19', '2000-03-21', 138);
+insert into students (first_name, last_name, patronymic, date_of_birth, gradesbook_number)
+values ('asd20', 'qwe20', 'zxc20', '2000-03-21', 139);
